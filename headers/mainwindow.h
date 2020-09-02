8473b55a57a889a9ea677e40259888dfb3a5665d
Denis - Home <dynaevdv@gmail.com>
2020-09-02 15:34:23 +0700

    [Step3] is ready

--- headers/mainwindow.h
+++ headers/mainwindow.h
@@ -9,8 +9,9 @@
 #include <QHBoxLayout>
 #include <QWidget>
 #include <QScrollArea>
-#include <QSpacerItem>
 #include <QComboBox>
+#include <QSpinBox>
+#include <QSizePolicy>
 
 #include <headers/filemanager.h>
 #include <headers/imagepyramid.h>
@@ -37,18 +38,27 @@ namespace ImageCompressor
         QLabel* m_activeLabel;
         ImagePyramid* m_activePyramide = nullptr;
 
-        int m_openImageButtonWidth = 100;
+        int m_heightOfControlBlock = 50;
+        int m_openImageButtonWidth = 130;
+        int m_openImageButtonHeight = 40;
         int m_layerComboBoxWidth = 100;
         int m_layerComboBoxHeight = 20;
-        int m_heightOfControlBlock = 35;
         int m_spaceBetweenElements = 20;
+        int m_comboboxWidth = 150;
 
         int m_numberOfFilterIteration = 3;
         int m_indexOfActiveLayer = 0;
         float m_scaleFactor = 2.0f;
 
-        void initControlBlock();
+        void initLayoutWithElements();
         void initImageBlock();
+        void initControlBlock();
+        void createOpenFileButton();
+        void createPyramideLayerCombobox();
+        void createScaleFactorSpinbox();
+        void createFilteringSpinbox();
+        void fillComboBoxes();
+
         void prepareWindowForNewImage();
         bool isNeedScrollbars(const QSize& imageSize);
     };
--- impl/mainwindow.cpp
+++ impl/mainwindow.cpp
@@ -17,7 +17,7 @@ void ImageCompressor::MainWindow::openAndShowImage()
 {
     auto sourcePixMap = FileManager::GetPixmapViaGui();
 
-    // If user closed GUI or something gone wrong
+    // Check for closed GUI or something gone wrong
     if (sourcePixMap != nullptr)
     {
         prepareWindowForNewImage();
@@ -29,7 +29,9 @@ void ImageCompressor::MainWindow::openAndShowImage()
         area->hide();
 
         // Create Pyramide of images
-        m_activePyramide = new ImagePyramid(sourcePixMap, m_numberOfFilterIteration, m_scaleFactor);
+        m_activePyramide = new ImagePyramid(sourcePixMap,
+                                            m_controlBlock->findChildren<QSpinBox*>("filterspinbox")[0]->value(),
+                                            m_controlBlock->findChildren<QDoubleSpinBox*>("scalespinbox")[0]->value());
 
         // Is need scrollarea or a simple label?
         if (isNeedScrollbars(sourcePixMap->size()))
@@ -53,16 +55,8 @@ void ImageCompressor::MainWindow::openAndShowImage()
             m_activeLabel = label;
         }
 
-        // Fill comboboxes
-        auto layercombobox = m_controlBlock->findChildren<QComboBox*>("layercombobox")[0];
-        layercombobox->blockSignals(true);
-        layercombobox->clear();
-        int numOfLayers = m_activePyramide->GetPyramideSize();
-        for(int i = 0; i < numOfLayers; i++)
-        {
-            layercombobox->addItem(QString::number(i));
-        }
-        layercombobox->blockSignals(false);
+        fillComboBoxes();
+
         m_imageBlock->show();
     }
 }
@@ -89,25 +83,9 @@ void ImageCompressor::MainWindow::initControlBlock()
 {
     m_controlBlock = new QWidget(this);
     m_controlBlock->setFixedSize(QSize(this->size().width(), m_heightOfControlBlock));
-    m_controlBlock->move(m_spaceBetweenElements, m_spaceBetweenElements);
     m_controlBlock->show();
 
-    // OpenFile button
-    auto openFile = new QPushButton(m_controlBlock);
-    openFile->setFixedSize(m_openImageButtonWidth, m_heightOfControlBlock);
-    openFile->setText("Open new image");
-    QObject::connect(openFile, SIGNAL (clicked(bool)),
-                     this, SLOT(openAndShowImage()));
-
-    openFile->show();
-
-    // Layer combobox
-    auto layerComboBox = new QComboBox(m_controlBlock);
-    layerComboBox->setObjectName("layercombobox");
-    layerComboBox->setFixedSize(m_layerComboBoxWidth, m_layerComboBoxHeight);
-    layerComboBox->move(m_spaceBetweenElements * 2 + m_openImageButtonWidth, m_heightOfControlBlock / 2 - m_layerComboBoxHeight / 2);
-    QObject::connect(layerComboBox, SIGNAL (currentIndexChanged(int)), this, SLOT (setPyramideLayer(int)));
-    layerComboBox->show();
+    initLayoutWithElements();
 }
 
 void ImageCompressor::MainWindow::initImageBlock()
@@ -134,6 +112,83 @@ void ImageCompressor::MainWindow::initImageBlock()
 
     area->setWidget(labelForScroll);
     area->hide();
+}
+
+void ImageCompressor::MainWindow::initLayoutWithElements()
+{
+    auto layout = new QHBoxLayout();
+    layout->setSpacing(m_spaceBetweenElements);
+    m_controlBlock->setLayout(layout);
+
+    createOpenFileButton();
+    createPyramideLayerCombobox();
+    createFilteringSpinbox();
+    createScaleFactorSpinbox();
+    layout->addSpacing(800);
+}
+
+void ImageCompressor::MainWindow::createOpenFileButton()
+{
+    auto openFile = new QPushButton();
+    openFile->setMinimumHeight(m_openImageButtonHeight);
+    openFile->setMinimumWidth(m_openImageButtonWidth);
+    m_controlBlock->layout()->addWidget(openFile);
+    openFile->setText("Open new image");
+    QObject::connect(openFile, SIGNAL (clicked(bool)),
+                     this, SLOT(openAndShowImage()));
+
+    openFile->show();
+}
+
+void ImageCompressor::MainWindow::createPyramideLayerCombobox()
+{
+    auto layerComboBox = new QComboBox();
+    m_controlBlock->layout()->addWidget(layerComboBox);
+    layerComboBox->setMinimumWidth(m_comboboxWidth);
+    layerComboBox->setObjectName("layercombobox");
+    QObject::connect(layerComboBox, SIGNAL (currentIndexChanged(int)), this, SLOT (setPyramideLayer(int)));
+    layerComboBox->show();
+}
+
+void ImageCompressor::MainWindow::createScaleFactorSpinbox()
+{
+    auto spinbox = new QDoubleSpinBox();
+    auto label = new QLabel("  ScaleFactor for compressing:");
+    m_controlBlock->layout()->addWidget(label);
+    m_controlBlock->layout()->addWidget(spinbox);
+    spinbox->setObjectName("scalespinbox");
+    spinbox->setValue(2.0);
+    spinbox->show();
+}
+
+void ImageCompressor::MainWindow::createFilteringSpinbox()
+{
+    auto spinbox = new QSpinBox();
+    auto label = new QLabel("  Number of gaussian filtering iterations on each compressing stage:");
+    m_controlBlock->layout()->addWidget(label);
+    m_controlBlock->layout()->addWidget(spinbox);
+    spinbox->setValue(3);
+    spinbox->setObjectName("filterspinbox");
+    spinbox->show();
+}
+
+void ImageCompressor::MainWindow::fillComboBoxes()
+{
+    // Layer combobox
+    auto layercombobox = m_controlBlock->findChildren<QComboBox*>("layercombobox")[0];
+    layercombobox->blockSignals(true);
+    layercombobox->clear();
+
+    for(int i = 0; i < m_activePyramide->GetPyramideSize(); i++)
+    {
+        layercombobox->addItem("Layer " +
+                               QString::number(i) + ":  " +
+                               QString::number(m_activePyramide->GetResolutionOfLayer(i).width()) +
+                               "x" +
+                               QString::number(m_activePyramide->GetResolutionOfLayer(i).height()));
+    }
+
+    layercombobox->blockSignals(false);
 }
 
 void ImageCompressor::MainWindow::prepareWindowForNewImage()
